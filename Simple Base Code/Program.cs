1de20c0018646cd12916fbf2a35600c091e350ca
Paul Lesieur <plesieur@jpkeefehs.org>
2021-10-12 13:23:00 -0400

    Add project files.

---
+++ Simple Base Code/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Simple_Base_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Class!");
+            Console.WriteLine("Don't forget to ass your test code here as well.");
+        }
+    }
+}
